18a35cfe5ca8620f3e729178b0c5170fac2eddd5
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-07-10 22:59:50 +0530

    init

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "../pages/Home";
+import Login from "../pages/Login";
+
+function App() {
+  return (
+    // <HomePage />
+    <Login/>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Greeting/style.js
@@ -0,0 +1,9 @@
+export const styleSheet = {
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        height: 'auto',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }
+}
